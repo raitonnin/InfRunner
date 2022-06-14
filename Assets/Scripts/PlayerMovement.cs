910ad0da4d007ab5ba20ac54a32567b7f1c942fe
raitonnin <45186265+raitonnin@users.noreply.github.com>
2022-06-14 14:28:07 -0500

    FirstCommit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody rb;
+    public float speed = 10.0f;
+    public float turnSpeed = 50.0f;
+    private Vector3 movementForce;
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        movementForce = new Vector3( x: horizontal, y: 0f, z: 0);
+         
+        
+    }
+    void Move()
+    {   
+        rb.AddForce(Vector3.forward * speed);
+        rb.AddForce(movementForce * turnSpeed);
+    }
+}
